fe248fa8cc32d3ea2adfef081153801a4496bcc1
Van Nguyen <van.nguyen2@stu.ocsb.ca>
2024-02-23 01:52:46 +0000

    Finished code that does math operators.

---
+++ MathOperators.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+// Addition
+let additionResult = 4 + 2
+print("4 + 2 = \(additionResult)")
+
+// Subtraction
+let subtractionResult = 4 - 2
+print("4 - 2 = \(subtractionResult)")
+
+// Multiplication
+let multiplicationResult = 4 * 2
+print("4 * 2 = \(multiplicationResult)")
+
+// Division (non-decimal)
+let divisionResult1 = 4 / 2
+print("4 / 2 = \(divisionResult1)")
+
+// Division (infinite decimal)
+let divisionResult2 = 2 / 3.0
+print("2 / 3 = \(divisionResult2)")
+
+// Exponent to the power of 2
+let exponentResult2 = pow(4, 2)
+print("4 ^ 2 = \(exponentResult2)")
+
+// Square root
+let squareRootResult = sqrt(9)
+print("Square root of 9 = \(squareRootResult)")
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Intro-05-Swift-MathOperators
+[![GitHub's Super Linter](https://github.com/ICS4U-Programming-VanN/Intro-05-Swift-MathOperators/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/ICS4U-Programming-VanN/Intro-05-Swift-MathOperators/actions)
